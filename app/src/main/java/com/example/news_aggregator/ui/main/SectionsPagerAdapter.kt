dc271b2c162c915071dff5d6e867e5f3dc4dac5a
FraserGrandfield <45278231+FraserGrandfield@users.noreply.github.com>
2020-10-11 16:54:49 +0100

    Tab layout and fragments.

---
+++ app/src/main/java/com/example/news_aggregator/ui/main/SectionsPagerAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.news_aggregator.ui.main
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.news_aggregator.R
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_text_1,
+    R.string.tab_text_2,
+    R.string.tab_text_3
+)
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
+    : FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        var fragment : Fragment = ForYou.newInstance()
+        when (position) {
+            0 -> {
+                fragment = ForYou.newInstance()
+            }
+            1 -> {
+                fragment = Trending.newInstance()
+            }
+            2 -> {
+                fragment = Local.newInstance()
+            }
+        }
+        return fragment
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        return 3
+    }
+}
