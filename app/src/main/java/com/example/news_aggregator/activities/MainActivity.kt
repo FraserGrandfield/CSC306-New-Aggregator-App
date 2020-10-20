92c32c9d3bec44934d4432e64cb606634a53030d
FraserGrandfield <45278231+FraserGrandfield@users.noreply.github.com>
2020-10-20 17:20:30 +0100

    Added more items to the menu. Swapped main screen and main activity around. Changed colour scheme.

--- app/src/main/java/com/example/news_aggregator/activities/MainActivity.kt
+++ app/src/main/java/com/example/news_aggregator/activities/MainActivity.kt
@@ -1,25 +1,22 @@
 package com.example.news_aggregator.activities
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.news_aggregator.R
-import com.example.news_aggregator.adapters.SectionsPagerAdapter
-import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
+    }
+
+    fun buttonOnClick(view: View) {
+        val intent = Intent(this, MainScreenActivity::class.java)
+        startActivity(intent)
     }
 
 }
