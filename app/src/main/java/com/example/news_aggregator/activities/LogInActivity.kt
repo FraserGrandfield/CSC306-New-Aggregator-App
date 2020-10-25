02c4927fed032b01af0674f4374e9ff501eca600
frasergrandfield <frasergrandfield110@gmail.com>
2020-10-25 20:29:09 +0000

    Designed login screen

--- app/src/main/java/com/example/news_aggregator/activities/LogInActivity.kt
+++ app/src/main/java/com/example/news_aggregator/activities/LogInActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.news_aggregator.R
+import kotlinx.android.synthetic.main.content_main.*
 
 
 class LogInActivity : AppCompatActivity() {
@@ -12,6 +13,10 @@ class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val toolbar = top_app_bar
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     fun buttonOnClick(view: View) {
