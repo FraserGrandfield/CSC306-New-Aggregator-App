4f0086f4ae8374643cc994d2dddebda2d1ce5846
frasergrandfield <frasergrandfield110@gmail.com>
2020-11-07 21:59:55 +0000

    Added Logout function. Navigation menu changes depending if your logged in or not.

--- app/src/main/java/com/example/news_aggregator/activities/MainActivity.kt
+++ app/src/main/java/com/example/news_aggregator/activities/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.news_aggregator.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuInflater
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -10,15 +12,18 @@ import androidx.viewpager.widget.ViewPager
 import com.example.news_aggregator.R
 import com.example.news_aggregator.adapters.SectionsPagerAdapter
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
-
 class MainActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mAuth = FirebaseAuth.getInstance()
+        Log.e("user", mAuth.currentUser.toString())
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
@@ -29,16 +34,23 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false);
         val drawerLayout = drawer
-        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav_draw, R.string.close_nav_draw)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.open_nav_draw,
+            R.string.close_nav_draw
+        )
         drawerLayout.addDrawerListener(toggle)
         toggle.isDrawerIndicatorEnabled = true
         toggle.syncState()
         val navigationView = nav_view
+        changeNavItems(navigationView.menu)
         navigationView.setNavigationItemSelectedListener { menuItem ->
             val id = menuItem.itemId
             if (id == R.id.log_out) {
-//                val intent = Intent(this, LogInActivity::class.java)
-//                startActivity(intent)
+                mAuth.signOut()
+                this.recreate()
             } else if (id == R.id.create_account) {
                 val intent = Intent(this, CreateAccountActivity::class.java)
                 startActivity(intent)
@@ -50,5 +62,20 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
+    }
+
+    private fun changeNavItems(menu : Menu) {
+        if (mAuth.currentUser == null) {
+            menu.getItem(0).isVisible = false
+            menu.getItem(1).isVisible = false
+            menu.getItem(2).isVisible = true
+            menu.getItem(3).isVisible = true
+
+        } else {
+            menu.getItem(0).isVisible = true
+            menu.getItem(1).isVisible = true
+            menu.getItem(2).isVisible = false
+            menu.getItem(3).isVisible = false
+        }
     }
 }
