cf5ed6fd6f8787ab65b4a9a2b4d85b66ba55f8f9
FraserGrandfield <45278231+FraserGrandfield@users.noreply.github.com>
2020-12-02 16:47:37 +0000

    Added a notification thing.

--- app/src/main/java/com/example/news_aggregator/activities/NotificationActivity.kt
+++ app/src/main/java/com/example/news_aggregator/activities/NotificationActivity.kt
@@ -1,8 +1,13 @@
 package com.example.news_aggregator.activities
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.news_aggregator.R
+import com.example.news_aggregator.services.NotificationService
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.content_main.top_app_bar
 
@@ -18,6 +23,10 @@ class NotificationActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         mAuth = FirebaseAuth.getInstance()
 
+        val component = ComponentName(baseContext, NotificationService::class.java)
+        val jobinfo = JobInfo.Builder(1, component).setPersisted(true).build()
+        val jobSchedular = this.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        jobSchedular.schedule(jobinfo)
     }
 
 }
