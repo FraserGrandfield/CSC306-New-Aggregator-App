b93a011ec6e54853440b1beda337234119b23c59
FraserGrandfield <45278231+FraserGrandfield@users.noreply.github.com>
2020-12-06 00:14:01 +0000

    Changed notification spinner to material menu.

--- app/src/main/java/com/example/news_aggregator/activities/NotificationActivity.kt
+++ app/src/main/java/com/example/news_aggregator/activities/NotificationActivity.kt
@@ -2,14 +2,20 @@ package com.example.news_aggregator.activities
 
 import android.app.AlarmManager
 import android.app.PendingIntent
+import android.content.ContentProvider
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.EditorInfo
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.view.get
 import com.example.news_aggregator.R
 import com.example.news_aggregator.services.NotificationReceiver
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_notifications.*
 import kotlinx.android.synthetic.main.content_main.top_app_bar
@@ -24,34 +30,31 @@ class NotificationActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         mAuth = FirebaseAuth.getInstance()
+        (notification_menu.editText as? AutoCompleteTextView)?.inputType = EditorInfo.TYPE_NULL
 
-        val spinner = spinner_notifications
-        ArrayAdapter.createFromResource(this,
-            R.array.notification_array,
-            android.R.layout.simple_spinner_item
-        ).also{ adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            spinner.adapter = adapter
-        }
+        //TODO makes these items a menu xml
+        val items = listOf("Never", "6 Hours", "12 Hours", "24 Hours")
+        val adapter = ArrayAdapter(this.applicationContext, R.layout.navigation_list_item, items)
+        (notification_menu.editText as? AutoCompleteTextView)?.setAdapter(adapter)
 
         val button = notification_button
         button.setOnClickListener {
-            val input = spinner_notifications.selectedItemPosition
+            val input = filled_exposed_dropdown.text.toString()
             var alarmTime = System.currentTimeMillis()
-            if (input == 1) {
+            if (input == "6 Hours") {
                 cancelAlarmManager()
                 startAlarmManager(alarmTime + 10000)
                 Log.e("notification", "1")
-            } else if (input == 2) {
+            } else if (input == "12 Hours") {
                 cancelAlarmManager()
                 startAlarmManager(alarmTime + 10000 * 2)
                 Log.e("notification", "2")
             }
-            else if (input == 3) {
+            else if (input == "24 Hours") {
                 cancelAlarmManager()
                 startAlarmManager(alarmTime + 10000 * 5)
                 Log.e("notification", "3")
-            } else if (input == 0) {
+            } else if (input == "Never") {
                 cancelAlarmManager() }
 
         }
